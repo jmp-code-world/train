b9c78a8cb0f895c08aa11322ac5f1db9fc665278
jm-paulin <jm-paulin@users.noreply.github.com>
2017-07-11 16:58:01 +0100

    Remove unwanted file

--- testupnp.js
+++
@@ -1,9 +0,0 @@
-var Server = require("upnpserver");
- 
-var server = new Server(
-  { httpPort: 10293 }, 
-  [
-    { path: '/home/jm/Documents/node/train/media' , mountPoint: '/Music' , type: 'music'  },
-  ]);
- 
-server.start();
